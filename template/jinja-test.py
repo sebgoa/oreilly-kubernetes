aa0bcdb25fdaf0d11ab658fc5222d1525116f18b
Sebastien Goasguen <runseb@gmail.com>
2018-01-19 05:44:13 +0100

    updatE

---
+++ template/jinja-test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import jinja2
+import kubernetes
+
+svc ="""
+apiVersion: batch/v2alpha1
+kind: CronJob
+metadata:
+  name: {{ name }}
+spec:
+  schedule: "*/1 * * * *"
+  jobTemplate:
+    spec:
+      template:
+        spec:
+          containers:
+          - name: kubecfg
+            image: kubecfg
+"""
+
+def render(name):
+    template = jinja2.Template(svc)
+    app = template.render(name=name)
+    return app.decode()
+
+print render("foobar")
